17ebbd137fc6c2e7728c0853de227d0bb3e7a23a
David Nascimento <davidpn.11@gmail.com>
2018-02-06 19:52:11 -0200

    pre merge

--- src/components/CommentsContainer/index.js
+++ src/components/CommentsContainer/index.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
-
+import AppBar from 'material-ui/AppBar'
+import BackIcon from 'material-ui/svg-icons/navigation/arrow-back'
+import IconButton from 'material-ui/IconButton'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 class CommentsList extends Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+  }
+
+  componentWillMount() {
+    const id = this.props.match.params.id
+    console.log(id)
+  }
+
   render() {
-    return <div>Hello</div>
+    console.log(this.props)
+    return (
+      <div>
+        <AppBar
+          title="Comments"
+          onTitleClick={this.handleFilterClick}
+          iconElementLeft={
+            <Link to={'/'}>
+              <IconButton>
+                <BackIcon color="#fff" />
+              </IconButton>
+            </Link>
+          }
+        />
+      </div>
+    )
   }
 }
 
